e097186b8226b8c80d4f7a10bafc901887be3300
Rahmat Arayyan <rahmatarayyan26@gmail.com>
2024-02-26 20:37:59 +0700

    belajar tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  theme: {
+    extend: {
+      spacing: {
+        13: "3.25rem",
+        15: "3.75rem",
+        128: "32rem",
+        144: "36rem",
+      },
+    },
+  },
+};
